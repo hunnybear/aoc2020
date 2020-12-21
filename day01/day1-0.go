2fb5fbd3ab8f96f4e5f3edaaa0b3db44b35f6675
Tyler Jachetta <tjachetta@imgix.com>
2020-12-20 20:50:44 -0500

    committing to move computers

--- day01/day1-0.go
+++ day01/day1-0.go
@@ -18,10 +18,13 @@ import (
 
 var re = regexp.MustCompile(`(\d+)-(\d+)\s+([a-zA-Z]):\s+([a-zA-Z]+)$`)
 
-func is_valid(line string) bool {
-	fmt.Println(line)
+type valid struct {
+	part_0 bool
+	part_1 bool
+}
+
+func is_valid(line string) valid {
 	res := re.FindAllStringSubmatch(line, -1)
-	fmt.Println(res)
 	min, err := strconv.Atoi(res[0][1])
 	if err != nil {
 		fmt.Println(line)
@@ -34,17 +37,32 @@ func is_valid(line string) bool {
 		fmt.Println(res)
 		log.Fatal(err)
 	}
-	count := strings.Count(res[0][4], res[0][3])
 
-	return min <= count && count <= max
+	target := res[0][3]
+	full_string := res[0][4]
 
-	return true
+	count := strings.Count(full_string, target)
+
+	// part 2 is min/max are 1-indexed indices. Valid == one and only one of the
+	// indicated indices indicates the input string
+
+
+
+	return valid{
+		part_0: min <= count && count <= max,
+		part_1: (full_string[min + 1] == target) != (full_string[max+1] == target)
+	}
+}
+
+func is_valid_part_1(line string) bool {
+
 }
 
 
 func main() {
 	file, err := os.Open("data")
-	valid := 0
+	valid_part_0 := 0
+	valid_part_1 := 0
 
 	if err != nil {
 		log.Fatal(err)
@@ -54,10 +72,16 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		if is_valid(scanner.Text()){
-			valid += 1
+		validity := is_valid(scanner.Text())
+		fmt.Println(validity)
+		if validity.part_0 {
+			valid_part_0 += 1
+		}
+		if validity.part_1 {
+			valid_part_1 += 1
 		}
 	}
 
-	fmt.Println(valid)
+	fmt.Println(valid_part_0)
+	fmt.Println(valid_part_1)
 }
