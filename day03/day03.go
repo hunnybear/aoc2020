d8337faa78c58129b8ce1fb3a3a9f73c26b80ef9
Tyler Jachetta <tjachetta@imgix.com>
2020-12-21 22:59:29 -0500

    Day 3: finished first part of day 3, learnded about bytes and shit

--- day03/day03.go
+++ day03/day03.go
@@ -1,8 +1,38 @@
 //usr/bin/env go run $0 $@ ; exit
+
+// right 3, down 1
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"log"
+//	"io"
+	"io/ioutil"
+	"unicode"
+)
+
+var open byte = '.'
+var tree byte = '#'
 
 func main() {
-	fmt.Println("whee")
+	trees := 0
+	
+	input, err := ioutil.ReadFile("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	the_map := bytes.Split(bytes.TrimFunc(input, unicode.IsSpace), []byte("\n"))
+
+	x := 0
+	
+	for y := 0; y < len(the_map); y ++ {
+		if the_map[y][x % len(the_map[y])] == tree {
+			trees ++
+		}
+
+		x += 3
+	}
+	fmt.Println(trees)
 }
