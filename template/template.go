e5b4e8d655512135fcd293d7856d09eefe6e6d20
Tyler Jachetta <tjachetta@imgix.com>
2020-12-25 14:32:18 -0500

    Template: Updated template to fit patterns

--- template/template.go
+++ template/template.go
@@ -4,11 +4,19 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 )
 
 func main() {
-	input, err := ioutil.ReadFile("input")
+	input_file := ""
+	
+	if len(os.Args) > 1  {
+		input_file = "test_input"
+	} else {
+		input_file = "input"
+	}
+	input, err := ioutil.ReadFile(input_file)
 	if err != nil {log.Fatal(err)}
 
 	fmt.Println(answer)
