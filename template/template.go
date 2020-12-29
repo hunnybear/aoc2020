9c0edaeca54267c0030c22cdd6f7bcb906a455c3
Tyler Jachetta <tjachetta@imgix.com>
2020-12-28 23:05:59 -0500

    Made template moar better

--- template/template.go
+++ template/template.go
@@ -6,18 +6,27 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 )
+
+const TEST_RESULT := 0
 
 func main() {
 	input_file := ""
 	
 	if len(os.Args) > 1  {
+		testing := true
 		input_file = "test_input"
 	} else {
+		testing := false
 		input_file = "input"
 	}
 	input, err := ioutil.ReadFile(input_file)
 	if err != nil {log.Fatal(err)}
 
 	fmt.Println(answer)
+
+	if testing && answer != TEST_RESULT {
+		log.Error(fmt.Sprintf"Test answer was wrong, got %d, expected %d", TEST_RESULT, answer)
+	}
 }
