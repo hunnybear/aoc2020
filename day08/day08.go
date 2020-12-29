fb3730a014604826d7dc793f34e06d6776bde2f5
Tyler Jachetta <tjachetta@imgix.com>
2020-12-28 23:06:27 -0500

    some WIP on day 8 part 2, moving on to 9 to get unstucks

--- day08/day08.go
+++ day08/day08.go
@@ -17,11 +17,134 @@ var JMP = []byte{'j', 'm', 'p'}
 var ACC = []byte{'a', 'c', 'c'}
 var NOP = []byte{'n', 'o', 'p'}
 
+type path []int
+
+
 type command struct {
 	// I had this as [3]byte, but that might just be me totally missing the point
 	instruction []byte
 	value int
 }
+
+type swap struct {
+	index int
+	instruction []byte
+}
+
+
+func get_commands(instruction []byte, commands []command) map[int]int {
+	res_map := make(map[int]int)
+	for cmd_i, command := range commands {
+		if bytes.Equal(command.instruction, instruction) {
+			res_map[cmd_i] = command.value
+		} 
+	}
+	return res_map
+}
+
+func filter_paths_by_origin(paths []path, origin int) []path {
+	fmt.Printf("filter start:\n%d\n\n", paths)
+	for i := len(paths) - 1; i >= 0; i-- {
+		if paths[i][0] != origin {
+			paths = append(paths[:i], paths[i+1:]...)
+		}
+	}
+	fmt.Printf("fitler res:\n%d\n\n", paths)
+	return paths
+}
+
+func find_swap(paths []path, commands []command, start int, jumps map[int]int) swap {
+	// Find the swap that will get from target starting position to one of the 
+	// provided paths
+
+	checked := make(map[int]bool)
+	//TOREMOVE
+	if len(checked) > 30{return swap{}}
+
+	for _, this_path := range paths {
+		for _, position := range this_path {
+			this_checked, ok := checked[position]
+			if this_checked == true {
+				continue
+			} else if ok == false {
+				fmt.Println(this_checked)
+			}
+
+			if bytes.Equal(commands[position-1].instruction, JMP) {
+				paths_to_prev := trace_paths(path{position-1}, commands, jumps)
+				
+				fmt.Printf("pre filter jmp -> acc (%d):\n%d\n", position, paths_to_prev)
+				paths_to_prev = filter_paths_by_origin(paths_to_prev, 0)
+
+				if len(paths_to_prev) > 1 {
+					log.Panic("I really don't know how")
+				} else if len(paths_to_prev) == 1 {
+					return swap {
+						index: position - 1,
+						instruction: ACC}
+				}
+			}
+
+			accs := get_commands(ACC, commands)
+			paths_to_prev := []path{}
+			for acc_idx, acc_val := range accs {
+				if acc_idx + acc_val == position {
+					paths_to_prev = append(paths_to_prev, trace_paths(path{acc_idx}, commands, jumps)...)
+
+				}
+			}
+			fmt.Printf("pre filter acc-> jmp:\n%d\n", paths_to_prev)
+			paths_to_prev = filter_paths_by_origin(paths_to_prev, 0)
+			if len(paths_to_prev) > 1 {
+				log.Panic("this be bad")
+			} else if len(paths_to_prev) == 1 {
+				return swap{index: paths_to_prev[0][len(paths_to_prev[0])-1],
+							instruction:JMP}
+			}
+
+			checked[position] = true
+		} 
+	}
+
+	return swap{}
+}
+
+
+func trace_paths(end path, commands []command, jumps map[int]int) []path {
+	paths := []path{}
+	target := end[0]
+
+	prev := []int{}
+
+	if ! bytes.Equal(commands[target-1].instruction, JMP) {
+		prev = append(prev, target-1)
+	}
+
+	for idx, jump_val := range jumps {
+		if idx + jump_val == end[0] {
+			fmt.Printf("%d hits %d (%d)\n", idx, end[0], jump_val)
+			prev = append(prev, idx) 
+		}
+	}
+
+	if len(prev) < 1 {
+		paths = append(paths, []int{})
+	}
+
+	for _, prev_idx := range prev {
+
+		for _, path_to_here := range trace_paths(path{prev_idx}, commands, jumps) {
+			// each path_to_here is its own path to here. sensibly.
+			fmt.Printf("path to here: %d\n\n", path_to_here)
+
+			paths = append(paths, append(path_to_here, end...))
+		}
+	}
+
+	return paths
+
+}
+
 
 func main() {
 	input_file := ""
@@ -40,6 +163,7 @@ func main() {
 	commands := make([]command, len(lines), len(lines))
 
 	
+
 	for i, line := range lines {
 		value, err := strconv.Atoi(string(line[4:]))
 		if err != nil {
@@ -50,6 +174,7 @@ func main() {
 			instruction: line[:3],
 			value: value}
 	}
+
 	seen := make(map[int]bool)
 	iterations := 0
 	for i:= 0; i < len(commands); i++ {
@@ -67,4 +192,15 @@ func main() {
 
 
 	fmt.Println(answer)
+
+	// part 2
+
+	jumps := get_commands(JMP, commands)
+
+	tails := trace_paths(path{len(commands)}, commands, jumps)
+	fmt.Println(tails)
+
+	res := find_swap(tails, commands, 0, jumps)
+	fmt.Println(res)
+
 }
